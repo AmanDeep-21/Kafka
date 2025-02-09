90df5c441c43d339126c49a63acec720fdb0fe77
AmanDeep-21 <amandeepsmile21@gmail.com>
2025-02-09 20:55:13 +0530

    added service annotation for KafkaConsumer

--- src/main/java/com/practice/kafka/serviceImpl/KafkaConsumer.java
+++ src/main/java/com/practice/kafka/serviceImpl/KafkaConsumer.java
@@ -2,7 +2,9 @@ package com.practice.kafka.serviceImpl;
 
 import com.practice.kafka.service.Consumer;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaConsumer implements Consumer {
 
     @KafkaListener(topics = "test-topic", groupId = "my-group")
